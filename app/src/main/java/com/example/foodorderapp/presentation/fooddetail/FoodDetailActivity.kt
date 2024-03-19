cb70f0fe5ab25b27f55420e0e2ced9ed328b4721
Daud Dhiya <daud.dhiya31@gmail.com>
2024-03-19 21:51:36 +0700

    feat: handle open url maps

--- app/src/main/java/com/example/foodorderapp/presentation/fooddetail/FoodDetailActivity.kt
+++ app/src/main/java/com/example/foodorderapp/presentation/fooddetail/FoodDetailActivity.kt
@@ -1,16 +1,21 @@
 package com.example.foodorderapp.presentation.fooddetail
 
 import android.annotation.SuppressLint
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.example.foodorderapp.R
 import com.example.foodorderapp.data.model.Food
 import com.example.foodorderapp.databinding.ActivityDetailBinding
 import com.example.foodorderapp.utils.toIndonesianFormat
+
 
 class FoodDetailActivity : AppCompatActivity() {
     private var totalPrice: Double = 0.0
     private var foodPrice: Double = 0.0
     private var totalFood: Int = 1
+    private val urlMaps: String = "https://maps.app.goo.gl/h4wQKqaBuXzftGK77"
 
     companion object {
         const val EXTRAS_ITEM = "EXTRAS_ITEM"
@@ -24,8 +29,12 @@ class FoodDetailActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
+
+        /* CUSTOM FUNCTION */
         handleBackBtn()
         handleCounterBtn()
+        handleOpenURLMaps()
+        /* =============== */
 
         /* GET ALL DATA */
         val data = intent.extras?.getParcelable<Food>(EXTRAS_ITEM)
@@ -56,7 +65,14 @@ class FoodDetailActivity : AppCompatActivity() {
         /* Set Content For ==== Detail Add To Cart ==== */
         binding.layoutAddToCart.btnAddToCart.text =
             "Tambah ke Keranjang - ${totalPrice.toIndonesianFormat()}"
+    }
 
+    private fun handleOpenURLMaps() {
+        binding.layoutDetailContent.layoutDetailLocation.tvLocationDesc.setOnClickListener {
+            val uri = Uri.parse(urlMaps)
+            val intent = Intent(Intent.ACTION_VIEW, uri)
+            startActivity(intent)
+        }
     }
 
     private fun handleCounterBtn() {
@@ -65,15 +81,15 @@ class FoodDetailActivity : AppCompatActivity() {
                 totalFood++
                 totalPrice = totalFood * foodPrice
                 binding.layoutAddToCart.btnAddToCart.text =
-                    "Tambah ke Keranjang - ${totalPrice.toIndonesianFormat()}"
+                    getString(R.string.tambah_ke_keranjang, totalPrice.toIndonesianFormat())
                 binding.layoutAddToCart.tvQtyFood.text = totalFood.toString()
             }
 
             it.btnDecrement.setOnClickListener {
-                if(totalFood > 1) totalFood--
+                if (totalFood > 1) totalFood--
                 totalPrice = totalFood * foodPrice
                 binding.layoutAddToCart.btnAddToCart.text =
-                    "Tambah ke Keranjang - ${totalPrice.toIndonesianFormat()}"
+                    getString(R.string.tambah_ke_keranjang, totalPrice.toIndonesianFormat())
                 binding.layoutAddToCart.tvQtyFood.text = totalFood.toString()
             }
         }
